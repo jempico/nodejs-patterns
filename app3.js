9cd7e7ded1ea83cf5499bd5239c5d45cb703099e
jempico <jpfilarca@gmail.com>
2021-03-16 00:03:43 +0100

    Add files via upload

--- app3.js
+++ app3.js
@@ -1,75 +1,60 @@
 const {
-    readdir,
-    readFile,
-    writeFile,
-  } = require("fs");
+  readdir,
+  readFile,
+  writeFile,
+} = require("fs");
+    
+const {join, resolve} = require("path");
   
-  const {util} = require("util");
+const inbox = join(__dirname, "inbox");
+const outbox = join(__dirname, "outbox");
   
-  const {join, resolve} = require("path");
-  
-  const inbox = join(__dirname, "inbox");
-  const outbox = join(__dirname, "outbox");
-  
-  const reverseText = str =>
-    str
-    .split("")
-    .reverse()
-    .join("");
+const reverseText = str =>
+  str
+  .split("")
+  .reverse()
+  .join("");
   
   
 //Proposal #3 to solve callback hell: with async await
 
-  function readDirectory(dirpath) {
-    return new Promise((resolve, reject) => {
-      readdir(dirpath, (error, files) => {
-         if (error) {
-           reject("Error: Folder inaccessible")
-        } else { 
-          resolve(files);
-        }
-      })
+const readDirectory = (dirpath) => {
+  return new Promise((resolve, reject) => {
+    readdir(dirpath, (error, files) => {
+      if (error) reject("Error: Folder inaccessible")
+      resolve(files);
     })
-  } 
-  
-  function copyData(file) {
-    return new Promise((resolve, reject) => {
-   
-        readFile(join(inbox, file), "utf8", (error, data) => {
-          if (error) { reject("Error: File error");
-          } else { 
-            resolve(data);
-          }
-        })
-    })
-  } 
-  
-  function transferData(data, newfile) {
-    return new Promise((resolve, reject) => {
-    
-    writeFile(join(outbox, newfile), reverseText(data), error => {
-      if (error) { reject("Error: File could not be saved!"); 
-      } else { 
-        resolve(`${newfile} was successfully saved in the outbox!`); 
-
-      } 
   })
-})
+} 
+  
+const copyData = (file) => {
+  return new Promise((resolve, reject) => {
+    readFile(join(inbox, file), "utf8", (error, data) => {
+      if (error) reject("Error: File error");
+      resolve(data);
+    })
+  })
+} 
+  
+const transferData = (data, newfile) => {
+  return new Promise((resolve, reject) => {
+    writeFile(join(outbox, newfile), reverseText(data), error => {
+      if (error) reject("Error: File could not be saved!"); 
+      resolve(`${newfile} was successfully saved in the outbox!`);  
+    })
+  })
 }
 
 
-async function reverseFiles(x) {
-    try {
-        let files = await readDirectory(x);
+( async function(x) {
+  try {
+    const files = await readDirectory(x);
+      files.forEach(async (file) => {
+        const copiedData = await copyData(file)
+          const transferedData = await transferData(copiedData, file)
+            console.log(transferedData);
+      })  
+    } catch (err) { console.log(err) }
+})(inbox) 
 
-        files.forEach(async (file) => {
-            const copiedFiles = await copyData(file)
-            const transferedFiles = await transferData(copiedFiles, file)
-            console.log(transferedFiles);
-        })  
-    } catch (err) {
-        console.log(err);
-    }   
-} 
 
-reverseFiles(inbox);
