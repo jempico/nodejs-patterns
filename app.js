6f2f07d9d96c27ecd78d0f6bab926828c3ffe22b
jempico <jpfilarca@gmail.com>
2021-03-15 15:59:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const {
-    readdir,
-    readFile,
-    writeFile,
-  } = require("fs");
- 
+  readdir,
+  readFile,
+  writeFile,
+} = require("fs");
+
 const {util} = require("util");
 
 const {join, resolve} = require("path");
 
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
-  
+
 const reverseText = str =>
-    str
-    .split("")
-    .reverse()
-    .join("");
+  str
+  .split("")
+  .reverse()
+  .join("");
 
 
-//Proposal #1 to solve callback hell: with callbacks.
+//Proposal #1 to solve callback hell: estructuring callbacks.
 
 function readDirectory(dirpath, readCallback) {
   readdir(dirpath, (error, files) => {
@@ -27,32 +27,37 @@ function readDirectory(dirpath, readCallback) {
     } else { 
         readCallback(files);
     }
-})
+  })
 } 
 
-function copyData(files, copyCallback) {
-  files.forEach(file => {
-    readFile(join(inbox, file), "utf8", (error, data) => {
-      if (error) { return console.log("Error: File error");
-      } else { 
-      copyCallback(data, file);
-      }
-    })
+function copyData(file, copyCallback) {
+  readFile(join(inbox, file), "utf8", (error, data) => {
+    if (error) { return console.log("Error: File error");
+    } else { 
+    copyCallback(data);
+    }
   })
 } 
 
 function transferData(data, newfile) {
-    writeFile(join(outbox, newfile), reverseText(data), error => {
-      if (error) { return console.log("Error: File could not be saved!"); 
-      } else { 
-      console.log(`${newfile} was successfully saved in the outbox!`); 
-      } 
+  writeFile(join(outbox, newfile), reverseText(data), error => {
+    if (error) { return console.log("Error: File could not be saved!"); 
+    } else { 
+    console.log(`${newfile} was successfully saved in the outbox!`); 
+    } 
   })
 }
 
 
-readDirectory(inbox, (files) => {
-  copyData(files, (data, newfile) => {
-    transferData(data, newfile)
-  })
+readDirectory(inbox, (files) => { 
+  files.forEach((file) => { 
+    copyData(file, (data) => { 
+      transferData(data, file)
+    }) 
+  }) 
 })
+
+
+
+
+
